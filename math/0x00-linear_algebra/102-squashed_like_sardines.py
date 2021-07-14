e4d9f0abf6a543e30848d3aed6f17dbf2b2c55ec
Naji Kadri <3214@holbertonschool.com>
2021-07-14 22:28:04 +0000

    fix sardines 2

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -1,37 +1,58 @@
 #!/usr/bin/env python3
-
-"""
-Module for cat_matrices.
-"""
-
-
-def get_length(rows):
-    """Recursive function returns list with length."""
-    if rows and (type(rows) is list or type(rows) is tuple):
-        return [len(rows), *get_length(rows[0])]
-    return []
-
-
-def matrix_shape(matrix):
-    """Calculates the shape of a matrix."""
-    return [*get_length(matrix)]
-
-
-def concatenate_rows(row1, row2, axis):
-    """Concatenates rows."""
-    if axis == 0:
-        return row1 + row2
-
-    range_row = range(len(row1))
-    return [concatenate_rows(row1[i], row2[i], axis - 1) for i in range_row]
+"""define new function"""
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    """ Concatenates two matrices along a specific axis."""
-    shape_matrix1 = matrix_shape(mat1)
-    shape_matrix2 = matrix_shape(mat2)
-
-    if shape_matrix1[axis + 1:] != shape_matrix2[axis + 1:]:
+    # def cat_matrices2D(mat1, mat2, axis=0):
+    """function that concatenates two matrices along a specific axis"""
+    # making deep copies
+    mat1 = [x[:] for x in mat1]
+    mat2 = [x[:] for x in mat2]
+    if len(matrix_shape(mat1)) != len(matrix_shape(mat2)):
         return None
+    if axis not in range(len(matrix_shape(mat1))):
+        return None
+    if axis == 0:
+        return cat_arrays(mat1, mat2)
+    elif axis == 1:
+        return list(map(lambda arr1, arr2: cat_arrays(arr1, arr2), mat1, mat2))
+    elif axis >= 2:
+        # return [cat_matrices(mat1[i], mat2[i], axis)
+        # for i in range(len(mat1))]
+        return "Hello World!!!"
 
-    return concatenate_rows(mat1, mat2, axis)
+
+def cat_arrays(arr1, arr2):
+    """function that adds two matrices element-wise"""
+    return arr1 + arr2
+
+
+def add_matrices(mat1, mat2):
+    """function that adds two matrices"""
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+    if len(matrix_shape(mat1)) == 1:
+        return [mat1[i] + mat2[i] for i in range(len(mat1))]
+    elif len(matrix_shape(mat1)) == 2:
+        return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
+                for i in range(len(mat1))]
+    # elif len(matrix_shape(mat1)) == 3:
+    #     return [[[mat1[i][j][k] + mat2[i][j][k]
+    # for k in range(len(mat1[0][0]))]
+    # for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    # elif len(matrix_shape(mat1)) == 4:
+    #     return [[[[mat1[i][j][k][l] + mat2[i][j][k][l]
+    # for l in range(len(mat1[0][0][0]))]
+    # for k in range(len(mat1[0][0]))]
+    # for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    elif len(matrix_shape(mat1)) >= 3:
+        return [add_matrices(mat1[i], mat2[i]) for i in range(len(mat1))]
+
+
+def matrix_shape(matrix):
+    """function that returns the shape of a matrix"""
+    shape = [len(matrix)]
+    while isinstance(matrix[0], list):
+        shape.append(len(matrix[0]))
+        matrix = matrix[0]
+    return shape
