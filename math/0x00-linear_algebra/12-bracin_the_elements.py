318f04dbc948f2145c640d94434fe89d74285b70
Naji Kadri <3214@holbertonschool.com>
2021-07-14 22:11:25 +0000

    Bracing The Elements

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+Module for Bracing The Elements
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """elementwise operations"""
+    return mat1+mat2, mat1-mat2, mat1*mat2, mat1/mat2
