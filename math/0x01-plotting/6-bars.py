016cae62095d01282365e9e22815866a2b9ff1d2
Naji Kadri <3214@holbertonschool.com>
2021-07-19 17:55:42 +0000

    Fix Bars

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(5)
-fruit = np.random.randint(0, 20, (4,3))
+fruit = np.random.randint(0, 20, (4, 3))
 
 fig, ax = plt.subplots()
 
@@ -14,15 +14,15 @@ index = np.zeros(3)
 bar_width = 0.5
 
 for i, n_fruits in enumerate(fruit):
-        ax.bar(
+    ax.bar(
             columns,
             n_fruits,
             bar_width,
             bottom=index,
             label=rows[i],
             color=colors[i]
-        )
-        index += n_fruits
+            )
+    index += n_fruits
 ax.set_ylabel('Quantity of Fruit')
 ax.set_ylim(0, 80)
 ax.set_title('Number of Fruit per Person')
