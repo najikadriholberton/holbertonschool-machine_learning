925bb625b6cc3ca7a9b8d9efcf1543218059a638
Naji Kadri <3214@holbertonschool.com>
2021-07-19 14:55:51 +0000

    Fix Freq

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -9,7 +9,7 @@ plt.xlabel("Grades")
 plt.ylabel("Number of Students")
 plt.title("Project A")
 plt.hist(student_grades, bins=np.arange(0, 110, 10),
-        edgecolor="black")
+         edgecolor="black")
 plt.xlim(0, 100)
 plt.ylim(0, 30)
 plt.xticks(np.arange(0, 110, 10))
